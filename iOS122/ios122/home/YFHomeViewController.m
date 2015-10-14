b6bb17fd632949014a4a97af60525785a686401b
颜风 <yanfengstyle@163.com>
2015-10-14 09:44:48 +0800

    添加一个xib复用的实例
    添加一个xib复用的实例

--- iOS122/ios122/home/YFHomeViewController.m
+++ iOS122/ios122/home/YFHomeViewController.m
@@ -9,7 +9,9 @@
 #import "YFHomeViewController.h"
 #import "YFAutoLayoutCellViewController.h"
 #import "YFAutoTransViewController.h"
+#import "YFPatternViewController.h"
 #import <AFNetworking.h>
+#import "YFXibDemoViewController.h"
 
 @interface YFHomeViewController ()<UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) UITableView * tabelView;
@@ -49,7 +51,7 @@
 #pragma mark - UITabelView 代理方法.
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 2;
+    return 3;
 }
 
 
@@ -64,6 +66,16 @@
     if (1 == indexPath.row) {
         cell.textLabel.text = @"UI设计图自动转iOS UI控件";
         cell.detailTextLabel.text = @"给出设计图,就能立即自动生成相应的代码.";
+    }
+    
+    if (2 == indexPath.row) {
+        cell.textLabel.text = @"Xib复用示例";
+        cell.detailTextLabel.text = @"Xib复用的实际例子";
+    }
+    
+    if (3 == indexPath.row) {
+        cell.textLabel.text = @"设计模式探寻";
+        cell.detailTextLabel.text = @"更加简化的Controller方案";
     }
     
     return cell;
@@ -81,6 +93,12 @@
         YFAutoTransViewController * autoTransVC = [[YFAutoTransViewController alloc] init];
         
         [self.navigationController pushViewController: autoTransVC animated: YES];
+    }
+    
+    if (2 == indexPath.row) {
+         YFXibDemoViewController * xibDemoVC = [[YFXibDemoViewController alloc] init];
+        
+        [self.navigationController pushViewController: xibDemoVC animated: YES];
     }
 }
 
