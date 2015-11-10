58887f185214511ab9e6b98358e4f2eda68a0e08
颜风 <yanfengstyle@163.com>
2015-11-10 09:51:07 +0800

    pod 'JSPatch' # 在线更新应用.
    pod 'JSPatch' # 在线更新应用.

--- iOS122/Pods/Target Support Files/Pods/Pods-resources.sh
+++ iOS122/Pods/Target Support Files/Pods/Pods-resources.sh
@@ -59,10 +59,12 @@ install_resource()
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "DateTools/DateTools/DateTools.bundle"
+  install_resource "JSPatch/JSPatch/JSPatch.js"
   install_resource "MJRefresh/MJRefresh/MJRefresh.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "DateTools/DateTools/DateTools.bundle"
+  install_resource "JSPatch/JSPatch/JSPatch.js"
   install_resource "MJRefresh/MJRefresh/MJRefresh.bundle"
 fi
 
