f6ba53b27107bf26a0e71ee1ba2c23d3de681b17
颜风 <yanfengstyle@163.com>
2015-11-23 14:36:45 +0800

    示例替换为 AwesomeProject 示例
    示例替换为 AwesomeProject 示例

--- iOS122/ReactComponent/iOS/index.ios.js
+++ iOS122/ReactComponent/iOS/index.ios.js
@@ -1,26 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
 'use strict';
 
 var React = require('react-native');
 var {
+  AppRegistry,
+  StyleSheet,
   Text,
-  View
+  View,
 } = React;
 
-var styles = React.StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red'
+var AwesomeProject = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
   }
 });
 
-class SimpleApp extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>This is a simple application.</Text>
-      </View>
-    )
-  }
-}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
-React.AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
--- iOS122/iOS122/samples/react/YFReactViewController.m
+++ iOS122/iOS122/samples/react/YFReactViewController.m
@@ -16,6 +16,7 @@
     [super viewDidLoad];
     
     NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/ReactComponent/iOS/index.ios.bundle?platform=ios&dev=true"];
+    
     // For production use, this `NSURL` could instead point to a pre-bundled file on disk:
     //
     //   NSURL *jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
@@ -23,7 +24,7 @@
     // To generate that file, run the curl command and add the output to your main Xcode build target:
     //
     //   curl http://localhost:8081/index.ios.bundle -o main.jsbundle
-    RCTRootView * rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation moduleName:@"SimpleApp" initialProperties:nil launchOptions:nil];
+    RCTRootView * rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation moduleName:@"AwesomeProject" initialProperties:nil launchOptions:nil];
     
     [self.view addSubview: rootView];
     
